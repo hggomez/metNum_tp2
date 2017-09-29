d0f2c7cd772019ee62f6cbf053ee4e19c8401622
javier <javiercortescondetito@gmail.com>
2017-09-29 18:07:41 -0300

    me

--- src/Test.cpp
+++ src/Test.cpp
@@ -10,32 +10,50 @@ using namespace std;
 //hacemos esto como primer idea ya que acuraccy tiene una cuenta muy parecida a las M
 
 void correr_Cross_Val(void (* metodo_resolucion)(Dato&, const vector<Dato>&, uint),const string& nombre_archivo, const string& nombre_metodo){
-    vector<int> etiquetas_Reales(10, 0);
+    //Lectura de datos
     ifstream k_fold_file;
     string aux = "../tests/"+nombre_archivo+ ".in";
     k_fold_file.open(aux);
 
     string data;
     int k_fold, k_nn, alpha;
+
     k_fold_file >> data;
     k_fold_file>> k_nn >> alpha >> k_fold;
     data += "train.csv";
-    vector<Mat<int> > lista_clasificadores(k_fold, Mat<int>(10,10,0));
-    vector<int> size_test(k_fold, 0);
-    vector<double> accuracy(k_fold, 0);
-    vector<double> recall(k_fold, 0);
-    vector<double> kappa(k_fold, 0);
-    vector<double> f_score(k_fold, 0);
     vector<Dato> datos;
     cerr<<data<<" "<<k_nn<<" "<<alpha<<" "<<k_fold<<endl;
     cargar_training(datos, data);
     if(alpha != 28*28){
 
     }
+    //guardo cuantas etiquetas hay de cada una en el set de test
+    ofstream etiquetas_kfold;
+    etiquetas_kfold.open(nombre_metodo+"_"+nombre_archivo+"_etiquetas.csv", ios_base::out);
+    //guardo las matrices de confusion
+    ofstream output;
+    output.open(nombre_metodo+"_"+nombre_archivo+".csv", ios_base::out);
+    output<<"tamaño_test,";
+    for(int i = 0;i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            output<<"("<<to_string(i)<<"."<<to_string(j)<<")";
+            if(i != 9 || j != 9 ) output<<",";
+
+        }
+    }
+    output <<"\n";
+
+    //me va a decir cuantas etiquetas de cada categoria hay en el set de test.
+    vector<int> etiquetas_Reales(10, 0);
+
+    //corro kfold
     for(int i = 0; i<k_fold; ++i){
         //cada fila habla del valor real de la etiqueta y cada columna acumula cuantos etiquetas seleccionamos mal/bien
+        //matriz de confusion
         Mat<int> matriz_etiquetas(10, 10, 0);
+        //subdivido el conjunto en training y test
         vector<Dato> training, test;
+
         //separo los datos en test y training, creo que tendria que saber las cantidades, no estoy seguro
         for(int j = 0; j<datos.size(); ++j){
             bool train;
@@ -48,7 +66,7 @@ void correr_Cross_Val(void (* metodo_resolucion)(Dato&, const vector<Dato>&, uin
                 etiquetas_Reales[datos[j].etiqueta]++;
             }
         }
-        size_test[i] = test.size();
+
         //corro la resolucion para todo lo que separamos como test.
         for(int j = 0; j< test.size(); ++j){
             Etiqueta real = test[j].etiqueta;
@@ -56,32 +74,14 @@ void correr_Cross_Val(void (* metodo_resolucion)(Dato&, const vector<Dato>&, uin
             matriz_etiquetas(real, test[j].etiqueta)++;
             cerr<<"cantidad_imagenes " <<test.size()<<" kfold "<<i<<" ya salio de la imagen "<<j<<"\n";
         }
-        lista_clasificadores[i] = matriz_etiquetas;
-    }
 
-    int filas = lista_clasificadores[0].filas();
-    int col = lista_clasificadores[0].columnas();
-    ofstream output;
-    output.open(nombre_metodo+" "+nombre_archivo+".csv", ios_base::out);
-    output<<"tamaño_test,";
-    for(int i = 0;i < filas; ++i) {
-        for (int j = 0; j < col; ++j) {
-            output<<"("<<to_string(i)<<"."<<to_string(j)<<")";
-            if(i != filas - 1 || j != col-1 ) output<<",";
-
-        }
-    }
-    output <<"\n";
-    for(int i = 0; i< k_fold; ++i){
-        cerr<<lista_clasificadores[i];
-        output<<size_test[i]<<",";
-        Mat<int> aux = lista_clasificadores[i];
-        for(int j = 0; j<filas; ++j){
-            for(int k = 0; k<col; ++k){
-                output<<aux(j,k);
-                if(j != filas-1 || k != col-1 ) output<<",";
+        for(int j = 0; j<10; ++j){
+            for(int k = 0; k<10; ++k){
+                output<< matriz_etiquetas(j,k);
+                if(j != 9 || k != 9 ) output<<",";
             }
         }
         output<<"\n";
     }
+
 }
