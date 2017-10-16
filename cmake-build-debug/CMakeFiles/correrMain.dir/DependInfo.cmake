7a99b4a169f233f4ee1ef8bad0cc9b8179b9985c
hggomez <horaciogomez.1993@gmail.com>
2017-10-16 00:09:38 -0300

    added makefile and its dependencies

---
+++ cmake-build-debug/CMakeFiles/correrMain.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/javito/metNum_tp2/src/KNN.cpp" "/home/javito/metNum_tp2/cmake-build-debug/CMakeFiles/correrMain.dir/src/KNN.cpp.o"
+  "/home/javito/metNum_tp2/src/Lectura.cpp" "/home/javito/metNum_tp2/cmake-build-debug/CMakeFiles/correrMain.dir/src/Lectura.cpp.o"
+  "/home/javito/metNum_tp2/src/PCA.cpp" "/home/javito/metNum_tp2/cmake-build-debug/CMakeFiles/correrMain.dir/src/PCA.cpp.o"
+  "/home/javito/metNum_tp2/src/Test.cpp" "/home/javito/metNum_tp2/cmake-build-debug/CMakeFiles/correrMain.dir/src/Test.cpp.o"
+  "/home/javito/metNum_tp2/src/main.cpp" "/home/javito/metNum_tp2/cmake-build-debug/CMakeFiles/correrMain.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/correrMain.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/correrMain.dir/src/KNN.cpp.o"
+  "CMakeFiles/correrMain.dir/src/Lectura.cpp.o"
+  "CMakeFiles/correrMain.dir/src/PCA.cpp.o"
+  "CMakeFiles/correrMain.dir/src/Test.cpp.o"
+  "CMakeFiles/correrMain.dir/src/main.cpp.o"
+  "correrMain.pdb"
+  "correrMain"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/correrMain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
